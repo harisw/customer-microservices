00d1801461fea13dd2dc45b9ea65378799336535
harisw <hari14@mhs.if.its.ac.id>
2022-05-27 19:26:15 +0900

    Add fraud microservices basecode - Add sequence and created table for fraud table database

---
+++ fraud/src/main/java/com/harisw/fraud/FraudApplication.java
@@ -0,0 +1,13 @@
+package com.harisw.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class FraudApplication {
+    public static void main(String[] args){
+        SpringApplication.run(FraudApplication.class, args);
+    }
+}
